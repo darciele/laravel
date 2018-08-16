cc1ad8431b02264bd90f5f18887506905a5f24c0
darcielesouza@gmail.com <darcielesouza@gmail.com>
2018-08-15 23:19:56 -0300

    aula laravel

---
+++ database/migrations/2018_08_16_005859_create_filmes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilmesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('filmes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('titulo');
+            $table->text('sinopse');
+            $table->string('genero');
+            $table->integer('duracao');
+
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('filmes');
+    }
+}
